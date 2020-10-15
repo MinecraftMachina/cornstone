0504580865d63ac5fc7c9028cf43db63444dddbb
Victor <virb3e@gmail.com>
2020-10-16 02:27:58 +0300

    docs: add example deployment

---
+++ examples/loader.bat
@@ -0,0 +1,62 @@
+@ECHO off
+
+rem ======================================================================================================
+rem CONFIGURATION
+rem ======================================================================================================
+
+SET "CORNSTONE_URL=https://github.com/MinecraftMachina/cornstone/releases/download/v1.0.0/cornstone_1.0.0_windows_amd64.exe"
+SET "MODPACK_NAME=Valhelsia 2"
+SET "MODPACK_URL=https://github.com/MinecraftMachina/valhelsia-2-corn/archive/master.zip"
+
+rem ======================================================================================================
+
+IF NOT EXIST "cornstone.exe" (
+    ECHO Downloading loader...
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('%CORNSTONE_URL%', '%~dp0cornstone.exe')" || GOTO :ERROR
+)
+
+:MENU
+CLS
+
+ECHO.
+ECHO ...............................................
+ECHO  %MODPACK_NAME% Launcher, powered by cornstone
+ECHO ...............................................
+ECHO.
+ECHO  1 - Install or update
+ECHO  2 - Play
+ECHO  3 - Play offline
+ECHO  4 - Exit
+ECHO.
+
+SET /P M=Type a number then press ENTER: 
+CLS
+
+IF %M%==1 GOTO :INSTALL
+IF %M%==2 GOTO :PLAY
+IF %M%==3 GOTO :OFFLINE
+IF %M%==4 GOTO :EXIT
+GOTO :MENU
+
+:INSTALL
+IF NOT EXIST "MultiMC" (
+    cornstone multimc -m "MultiMC" init || GOTO :ERROR
+)
+cornstone multimc -m "MultiMC" install -u -n "%MODPACK_NAME%" -i "%MODPACK_URL%" || GOTO :ERROR
+GOTO :MENU
+
+:PLAY
+cornstone multimc -m "MultiMC" run
+GOTO :EXIT
+
+:OFFLINE
+cornstone multimc -m "MultiMC" offline
+GOTO :PLAY
+
+:EXIT
+exit
+
+:ERROR
+ECHO Failed with error #%errorlevel%.
+pause
+exit /b %errorlevel%
---
+++ examples/loader.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+set -e
+
+# ======================================================================================================
+# CONFIGURATION
+# ======================================================================================================
+
+CORNSTONE_URL="https://github.com/MinecraftMachina/cornstone/releases/download/v1.0.0/cornstone_1.0.0_linux_amd64"
+MODPACK_NAME="Valhelsia 2"
+MODPACK_URL="https://github.com/MinecraftMachina/valhelsia-2-corn/archive/master.zip"
+
+# ======================================================================================================
+
+if [ ! -f "cornstone" ]; then
+    echo "Downloading loader..."
+    curl -L -s "$CORNSTONE_URL" -o "cornstone"
+fi
+
+function MENU {
+    clear
+
+    echo ""
+    echo "..............................................."
+    echo " $MODPACK_NAME Launcher, powered by cornstone"
+    echo "..............................................."
+    echo ""
+    echo " 1 - Install or update"
+    echo " 2 - Play"
+    echo " 3 - Play offline"
+    echo " 4 - Exit"
+    echo "" 
+    read -p $'Type a number then press ENTER: ' CHOICE
+
+    clear
+
+    case $CHOICE in
+        "1")
+            INSTALL ;;
+        "2")
+            PLAY ;;
+        "3")
+            OFFLINE ;;
+        "4")
+            EXIT ;;
+        *) MENU ;;
+    esac
+}
+
+function INSTALL {
+    if [ ! -d "MultiMC" ]; then
+        $PWD/cornstone multimc -m "MultiMC" init
+    fi
+    $PWD/cornstone multimc -m "MultiMC" install -u -n "$MODPACK_NAME" -i "$MODPACK_URL"
+    MENU
+}
+
+function PLAY {
+    $PWD/cornstone multimc -m "MultiMC" run
+    MENU
+}
+
+function OFFLINE {
+    $PWD/cornstone multimc -m "MultiMC" offline
+    PLAY
+}
+
+function EXIT {
+    exit
+}
+
+MENU
